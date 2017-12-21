71c559ae379640e6e4374880001f6355f2558c81
杜大文 <34057787+nsdsb@users.noreply.github.com>
2017-12-21 18:52:36 +0800

    day22

---
+++ Day17_练习项目/src/utils/Constants.java
@@ -0,0 +1,17 @@
+package utils;
+
+/**
+ * Created by zyf on 2017/12/14.
+ */
+public class Constants {
+
+	public static void main(String[] args) {
+		NetTool.getInstance();
+	}
+
+	public static final String URL_BASE = "http://192.168.20.194:8080";
+
+	public static final String URL_INSERT = URL_BASE+"/day16/insert";
+	public static final String URL_FIRST = URL_BASE+"/day16/first";
+	public static final String URL_TEN = URL_BASE+"/day16/ten";
+}
